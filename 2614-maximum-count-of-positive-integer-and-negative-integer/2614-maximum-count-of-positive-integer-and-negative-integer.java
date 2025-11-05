fce77b54b7618d943e2b9cc8592fa0164f7d1515
BabluChahar <141544824+BabluChahar@users.noreply.github.com>
2025-11-05 21:10:22 +0530

    Time: 0 ms (100%), Space: 46.8 MB (7.79%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,43 +1,29 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int k=firstNegative(nums)+1;
-        int l=nums.length-firstPositive(nums);
-        return Math.max(k,l);
-        
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>0){
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+           
+        }
+         int pos=n-low;
+        low=0;
+        high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=0){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        int neg=low;
+        return Math.max(pos,neg);
     }
-     public int firstNegative(int []nums){
-        int start=0;
-        int end=nums.length-1;
-        int ans=-1;
-        while(end>=start){
-            int mid=start+(end-start)/2;
-            
-                if(nums[mid]<0){
-                    ans=mid;
-                    start=mid+1;
-                }
-                else{
-                      end=mid-1;
-            }
-        }
-  return ans;
-        }
-    public int firstPositive(int []nums){
-        int start=0;
-        int end=nums.length-1;
-        int ans=nums.length;
-        while(end>=start){
-            int mid=start+(end-start)/2;
-            
-                if(nums[mid]>0){
-                    ans=mid;
-                    end=mid-1;
-                }
-                else{
-                start=mid+1;
-                }
-            }
-  return ans;
-        }
-    
 }
